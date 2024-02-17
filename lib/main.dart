38deb1df893df5c92d248d9140d1b89054db790c
phanousith1 <tomatolandz1@gmail.com>
2024-02-17 16:47:14 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_application_1/MyWidget2.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyWidget2(),
+    );
+  }
+}
